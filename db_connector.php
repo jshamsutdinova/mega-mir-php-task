06e711129f517cf4b41d7e4b11212a5f14b03431
Julia Shamsutdinova <jshamsutdinova@localhost.localdomain>
2022-04-25 14:56:08 +0800

    Uploaded db connector

---
+++ db_connector.php
@@ -0,0 +1,31 @@
+<?php
+
+function openConnDB() {
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "password";
+    $db = "records";
+
+    $connDB = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+
+    if($connDB == false) {
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+
+    return $connDB;
+}
+
+function createTable($conn) {
+
+    $sql = "CREATE TABLE records(
+            `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            `user_name` VARCHAR(30) NOT NULL,
+            `message` VARCHAR(100) NOT NULL,
+            `date` DATE NOT NULL
+        )";
+
+    mysqli_query($conn, $sql);
+
+}
+
+?>
